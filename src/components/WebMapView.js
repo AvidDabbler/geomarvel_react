6f7555187de0e4a914fd432ef0e56f46bee97295
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-04 10:55:41 -0600

    cors patch

--- src/components/WebMapView.js
+++ src/components/WebMapView.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { loadModules } from 'esri-loader';
+import getTrees from '../hooks/getTrees';
 
 const wardConfig = require('../mapConfig/wardConfig.json')
 
@@ -7,59 +8,62 @@ const wardConfig = require('../mapConfig/wardConfig.json')
 
 export const WebMapView = () => {
   const mapRef = useRef();
+
+  const loadMap = () => {
+    // lazy load the required ArcGIS API for JavaScript modules and CSS
+    loadModules([
+      'esri/Map', 
+      'esri/views/MapView',
+      "esri/layers/GeoJSONLayer",
+      "esri/layers/support/Field",
+      
+    ], { css: true })
+    .then(([
+      ArcGISMap, 
+      MapView, 
+      GeoJSONLayer, 
+      Field,
+    ]) => {
+      const map = new ArcGISMap({
+        basemap: 'topo-vector'
+      });
+      
+      // load the map view at the ref's DOM node
+      const view = new MapView({
+        container: mapRef.current,
+        map: map,
+        center: [-77.03738075521436, 38.89504310519569],
+        zoom: 12
+      });
+      
+      // const fieldList = (fields) => {
+      //     return fields.forEach(field => {
+      //       new Field(field)
+      //     });
+      // }
+
+        // START OF WARD CONFIG
+
+        // var WardFields = fieldList(wardConfig.fields);
+
+        const wards = new GeoJSONLayer(wardConfig.layer);
+
+        // END OF WARD CONFIG
+       
+        map.add(wards)
+        getTrees()
+
+        return () => {
+          if (view) {
+            // destroy the map view
+            view.destroy();
+          }
+        };
+      });
+    }
   
   useEffect(
-    () => {
-      // lazy load the required ArcGIS API for JavaScript modules and CSS
-      loadModules([
-        'esri/Map', 
-        'esri/views/MapView',
-        "esri/layers/GeoJSONLayer",
-        "esri/layers/support/Field",
-        
-      ], { css: true })
-      .then(([
-        ArcGISMap, 
-        MapView, 
-        GeoJSONLayer, 
-        Field,
-      ]) => {
-        const map = new ArcGISMap({
-          basemap: 'topo-vector'
-        });
-        
-        // load the map view at the ref's DOM node
-        const view = new MapView({
-          container: mapRef.current,
-          map: map,
-          center: [-77.03738075521436, 38.89504310519569],
-          zoom: 12
-        });
-        
-        // const fieldList = (fields) => {
-        //     return fields.forEach(field => {
-        //       new Field(field)
-        //     });
-        // }
-
-          // START OF WARD CONFIG
-
-          // var WardFields = fieldList(wardConfig.fields);
-
-          const wards = new GeoJSONLayer(wardConfig.layer);
-
-          // END OF WARD CONFIG
-         
-          map.add(wards)
-
-          return () => {
-            if (view) {
-              // destroy the map view
-              view.destroy();
-            }
-          };
-        });
-      }
+    loadMap,
     );
 
     return <div className="webmap" ref={mapRef} />;
---
+++ src/hooks/getTrees.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import React, { useEffect, useState} from 'react';
+
+import cors from '../cors'
+
+const API_URL = cors("http://localhost:3000")
+
+const getTrees = () => {
+    var config = {
+        method: 'GET',
+        url: `${API_URL}getAll`,
+        dataType: "json",
+        contentType: "application/json",
+    };
+    axios(config).then(function (response) {
+        console.log(response.data);
+    }).catch(function (error) {
+        console.error(error);
+    });
+};
+
+
+export default getTrees;
