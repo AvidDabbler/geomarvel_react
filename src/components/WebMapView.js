e82fd9d34c16bc516db133bf49e6d04da1161c19
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-04 09:32:00 -0600

    ward styling and cleanup

--- src/components/WebMapView.js
+++ src/components/WebMapView.js
@@ -1,60 +1,54 @@
 import React, { useEffect, useRef } from 'react';
 import { loadModules } from 'esri-loader';
 
+const wardConfig = require('../mapConfig/wardConfig.json')
+
+
 
 export const WebMapView = () => {
-    const mapRef = useRef();
+  const mapRef = useRef();
+  
+  useEffect(
+    () => {
+      // lazy load the required ArcGIS API for JavaScript modules and CSS
+      loadModules([
+        'esri/Map', 
+        'esri/views/MapView',
+        "esri/layers/GeoJSONLayer",
+        "esri/layers/support/Field",
+        
+      ], { css: true })
+      .then(([
+        ArcGISMap, 
+        MapView, 
+        GeoJSONLayer, 
+        Field,
+      ]) => {
+        const map = new ArcGISMap({
+          basemap: 'topo-vector'
+        });
+        
+        // load the map view at the ref's DOM node
+        const view = new MapView({
+          container: mapRef.current,
+          map: map,
+          center: [-77.03738075521436, 38.89504310519569],
+          zoom: 12
+        });
+        
+        // const fieldList = (fields) => {
+        //     return fields.forEach(field => {
+        //       new Field(field)
+        //     });
+        // }
 
-    useEffect(
-      () => {
-        // lazy load the required ArcGIS API for JavaScript modules and CSS
-        loadModules([
-            'esri/Map', 
-            'esri/views/MapView',
-            "esri/layers/GeoJSONLayer",
-            "esri/layers/support/Field",
+          // START OF WARD CONFIG
 
-        ], { css: true })
-        .then(([
-            ArcGISMap, 
-            MapView, 
-            GeoJSONLayer, 
-            Field,
-            PopupTemplate,
-        ]) => {
-          const map = new ArcGISMap({
-            basemap: 'topo-vector'
-          });
+          // var WardFields = fieldList(wardConfig.fields);
 
-          // load the map view at the ref's DOM node
-          const view = new MapView({
-            container: mapRef.current,
-            map: map,
-            center: [-77.03738075521436, 38.89504310519569],
-            zoom: 12
-          });
+          const wards = new GeoJSONLayer(wardConfig.layer);
 
-          var WardFields = [
-            new Field({
-              "name": "ObjectID",
-              "alias": "ObjectID",
-              "type": "oid"
-            }), new Field({
-              "name": "WARD",
-              "alias": "Ward",
-              "type": "string"
-            })
-           ];
-
-          const wards = new GeoJSONLayer({
-            url: "https://opendata.arcgis.com/datasets/0ef47379cbae44e88267c01eaec2ff6e_31.geojson",
-            fields: WardFields,
-            popupEnabled: true,
-            popupTemplate: {
-              title: "Ward: {WARD}",
-              
-            }}
-            );
+          // END OF WARD CONFIG
          
           map.add(wards)
 
