3c27b13c466226b18b1aef794c46901db1a7915b
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-05 12:22:59 -0600

    ButtonsGroup WORKS!!!

--- src/components/ButtonsGroup.js
+++ src/components/ButtonsGroup.js
@@ -1,24 +1,23 @@
 import React, {useState} from 'react';
 
 const ButtonsGroup = (props) => {
-    const buttons = () => {
-        return(
-            props.list.forEach((item) => {
-                    <li class="mr-3">
-                        <a 
-                            key={item} 
-                            className="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" 
-                            href="#">{item}
-                        </a>
-                    </li>
-            })
-        )
-    }
+    let elements = [];
+    
+    props.list.forEach((item) => {
+        elements.push(
+            <li class="mr-3">
+                <a 
+                    key={item} 
+                    className="inline-block border border-blue-500 rounded py-1 px-3 bg-blue-500 text-white" 
+                    href="#">{item}
+                </a>
+            </li>)
+    })
     
     return(
         <div>
             <ul>
-                {buttons()}
+                {elements}
                 <li>here</li>
             </ul>
         </div>
