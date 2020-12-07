a2298d55f25bfe95965e9499651e27bc5430a90b
AvidDabbler <walter.k.jenkins@gmail.com>
2020-12-07 08:50:11 -0600

    separated loadMap and addLayer

--- src/components/WebMapView.js
+++ src/components/WebMapView.js
@@ -14,127 +14,141 @@ export const WebMapView = () => {
   const mapRef = useRef();
   const [treeURL, setTreeURL] = TreeURL();
 
-  const loadMap = (treeURL) => { 
+  const loadMap = async () => { 
     // lazy load the required ArcGIS API for JavaScript 
     // modules and CSS
-    loadModules([
-      'esri/Map', 
-      'esri/views/MapView',
-      "esri/layers/GeoJSONLayer",
-      "esri/smartMapping/renderers/type",
-      "esri/core/watchUtils",
-      "esri/renderers/UniqueValueRenderer",
-      "dojo/dom",
-      "dojo/domReady!"
-    ], { css: true })
-    .then(([
-      ArcGISMap, 
-      MapView, 
-      GeoJSONLayer, 
-      typeRendererCreator,
-      watchUtils,
-      UniqueValueRenderer,
-      dom
-    ]) => {
-      const map = new ArcGISMap({
-        basemap: 'topo-vector'
-      });
-      
-      // load the map view at the ref's DOM node
-      const view = new MapView({
-        container: mapRef.current,
-        map: map,
-        center: [-77.03, 38.89],
-        zoom: 12,
-        popup: {
-          dockEnabled: true,
-          dockOptions: {
-            buttonEnabled: false,
-            breakpoint: false
-          }
+    // loadModules([
+    //   'esri/Map', 
+    //   'esri/views/MapView',
+    //   "esri/smartMapping/renderers/type",
+    //   "esri/core/watchUtils",
+    //   "esri/renderers/UniqueValueRenderer",
+    //   "dojo/dom",
+    //   "dojo/domReady!"
+    // ], { css: true })
+    // .then(([
+    //   ArcGISMap, 
+    //   MapView, 
+    //   GeoJSONLayer, 
+    //   typeRendererCreator,
+    //   watchUtils,
+    //   UniqueValueRenderer,
+    //   dom
+    // ]) => {
+    const [ArcGISMap, MapView] = await loadModules(['esri/Map', 'esri/views/MapView']);
+    
+    const map = new ArcGISMap({
+      basemap: 'topo-vector'
+    });
+
+    // load the map view at the ref's DOM node
+    const view = new MapView({
+      container: mapRef.current,
+      map: map,
+      center: [-77.03, 38.89],
+      zoom: 12,
+      popup: {
+        dockEnabled: true,
+        dockOptions: {
+          buttonEnabled: false,
+          breakpoint: false
         }
-      });
-      
-      const wards = new GeoJSONLayer(wardConfig.layer);
-      const trees = new GeoJSONLayer(TreeConfig(treeURL));
-      
-
-      const generateRenderer = () => {
-        // configure parameters for the color renderer generator.
-        // The layer must be specified along with a field name
-        // The view and other properties determine
-        // the appropriate default color scheme.
-
-        var typeParams = {
-          layer: trees,
-          view: view,
-          field: "CONDITION",
-          legendOptions: {
-            title: "Last known condition of tree"
-          }
-        };
-
-        typeRendererCreator.createRenderer(typeParams)
-          .then(function (response) {
-            // set the renderer to the layer and add it to the map
-
-            trees.renderer = response.renderer;
-
-            if (!map.layers.includes(trees)) {
-              map.add(trees);
-            }
-          })
-          .catch(function (error) {
-            console.error("there was an error: ", error);
-          });
       }
-      // watchUtils.whenFalseOnce(view, "updating", generateRenderer);
+    });
 
-      // start of click event
-      // https://totalapis.github.io/sample-code/sandbox/index.html?sample=view-hittest
-      view.on("click", (event)=>clickFeature(event, view));
+    return {map, view}
+  };
+  
+  const addLayer = (map, config) => {
+    loadModules([
+      "esri/layers/GeoJSONLayer",
+    ], { css: true })
+    .then(([GeoJSONLayer])=>{
+      map.add(new GeoJSONLayer(config))
+    })
+  }
 
-
-      // access the attributes
-      watchUtils.whenFalseOnce(view, "updating", generateRenderer); 
-
-      // END OF WARD CONFIG
-      map.add(wards)
-      map.add(trees)
+      // const wards = new GeoJSONLayer(wardConfig.layer);
+      // const trees = new GeoJSONLayer(TreeConfig(treeURL));
       
 
-      // view.constraints = {minZoom: 12};
+      // const generateRenderer = () => {
+      //   // configure parameters for the color renderer generator.
+      //   // The layer must be specified along with a field name
+      //   // The view and other properties determine
+      //   // the appropriate default color scheme.
 
-        return () => {
-          if (view) {
-            // destroy the map view
-            view.destroy();
-          }
-        };
-      });
-    }
-  const updateData = async () => {
-    let json;
-    var config = {
-        method: 'GET',
-        url: `http://localhost:3000/getAll`,
-        dataType: "json",
-        contentType: "application/json",
-    };
+      //   var typeParams = {
+      //     layer: trees,
+      //     view: view,
+      //     field: "CONDITION",
+      //     legendOptions: {
+      //       title: "Last known condition of tree"
+      //     }
+      //   };
 
-    axios(config)
-    .then(function (response) {
-      console.log(response.data)
-      json = response.data;
-    }).catch(function (error) {
-        console.error(error);
-    })
-    return await json
-};
+      //   typeRendererCreator.createRenderer(typeParams)
+      //     .then(function (response) {
+      //       // set the renderer to the layer and add it to the map
+
+      //       trees.renderer = response.renderer;
+
+      //       if (!map.layers.includes(trees)) {
+      //         map.add(trees);
+      //       }
+      //     })
+      //     .catch(function (error) {
+      //       console.error("there was an error: ", error);
+      //     });
+      // }
+      // // watchUtils.whenFalseOnce(view, "updating", generateRenderer);
+
+      // // start of click event
+      // // https://totalapis.github.io/sample-code/sandbox/index.html?sample=view-hittest
+      // view.on("click", (event)=>clickFeature(event, view));
+
+
+      // // access the attributes
+      // watchUtils.whenFalseOnce(view, "updating", generateRenderer); 
+
+      // // END OF WARD CONFIG
+      // map.add(wards)
+      // map.add(trees)
+      
+
+      // // view.constraints = {minZoom: 12};
+
+      //   return () => {
+      //     if (view) {
+      //       // destroy the map view
+      //       view.destroy();
+      //     }
+      //   };
+      // });
+
+    const updateData = async () => {
+      let json;
+      var config = {
+          method: 'GET',
+          url: `http://localhost:3000/getAll`,
+          dataType: "json",
+          contentType: "application/json",
+      };
+
+      axios(config)
+      .then(function (response) {
+        console.log(response.data)
+        json = response.data;
+      }).catch(function (error) {
+          console.error(error);
+      })
+      return await json
+  };
   
   useEffect(async()=>{
-    loadMap(treeURL)
-    
+      const{map, view} = await loadMap()
+      addLayer(map, TreeConfig(treeURL))
+
   });
 
     return <div className="webmap" ref={mapRef} />;
